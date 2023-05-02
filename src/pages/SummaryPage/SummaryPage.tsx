f478d66beb21d2eda2505de681f8943b34c7d95a
gjlee <gyujin89@gmail.com>
2023-05-02 10:30:36 +0900

    feat : 주문 요약 페이지 추가

--- src/pages/SummaryPage/SummaryPage.tsx
+++ src/pages/SummaryPage/SummaryPage.tsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+
 export default function SummaryPage() {
-  return <div>요약 페이지</div>;
+  const [checked, setChecked] = useState(false);
+
+  const handleChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+  };
+
+  return (
+    <div>
+      <form>
+        <input type="checkbox" id="confirm-checkbox" checked={checked} onChange={handleChangeCheckbox} />{' '}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button disabled={!checked} type="submit">
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/tests/Shop/SummaryPage/SummaryPage.test.tsx
+++ src/tests/Shop/SummaryPage/SummaryPage.test.tsx
@@ -7,4 +7,7 @@ test('checkbox and button', () => {
     name: '주문하려는 것을 확인하셨나요?',
   });
   expect(checkbox.checked).toEqual(false);
+
+  const btnConfirm: HTMLButtonElement = screen.getByRole('button', { name: '주문 확인' });
+  expect(btnConfirm.disabled).toBeTruthy();
 });
